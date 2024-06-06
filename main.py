fe3b3f29c1c55cc0a8fc9cd899eef3ab07030248
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-06 13:47:27 -0400

    Don't need to use `GET` right now (might use for other things later)

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ class MyHandler(BaseHTTPRequestHandler):
 		self.send_header('Access-Control-Allow-Origin', '*')
 		BaseHTTPRequestHandler.end_headers(self)
 		
-	def do_GET(self):
-		try:
-			with open('templates/index2.html', 'r') as f:
-				content = f.read()
-			self.send_response(200)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
-			self.wfile.write(bytes(content, 'utf8'))
-		except:
-			self.send_response(404)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
-			self.wfile.write(b'404: File not found')
+	# def do_GET(self):
+	# 	try:
+	# 		with open('templates/index2.html', 'r') as f:
+	# 			content = f.read()
+	# 		self.send_response(200)
+	# 		self.send_header('Content-type', 'text/html')
+	# 		self.end_headers()
+	# 		self.wfile.write(bytes(content, 'utf8'))
+	# 	except:
+	# 		self.send_response(404)
+	# 		self.send_header('Content-type', 'text/html')
+	# 		self.end_headers()
+	# 		self.wfile.write(b'404: File not found')
 
 	def do_POST(self):
 		try:
