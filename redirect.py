656a50df817f5c8cedb9d4e953d796a57b62a63a
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-10 21:35:41 -0400

    Better errors

--- redirect.py
+++ redirect.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-import cgi#, cgitb
+print("Content-type: text/html")
+print()
+
+import cgi, cgitb
 
 from send import send
 
-# cgitb.enable()
+cgitb.enable()
 
 IP = "localhost"
 PORT = 8001
@@ -12,6 +15,4 @@ PORT = 8001
 data = cgi.FieldStorage().value
 send(IP, PORT, data)
 
-print("Content-type: text/html")
-print()
 print("")
