2df7934b361641bd7c550600110385ab87f207fc
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-11 16:23:16 -0400

    Bytes and prepended backslash

--- redirect.py
+++ redirect.py
@@ -13,6 +13,9 @@ IP = "localhost"
 PORT = 8001
 
 data = cgi.FieldStorage().value
+data = bytes(data, 'utf-8')
+data = data.split(b'\\')
+data = b'\\' + int(data[0]).to_bytes(1,"little") + data[1]
 send(IP, PORT, data)
 
 print("")
