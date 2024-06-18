11fd72f1aef25a74b054f1748a9f3e25625e652f
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-18 13:35:03 -0400

    Better errors given codes from Java

--- send.py
+++ send.py
@@ -2,16 +2,24 @@ import socket
 import traceback
 from datetime import datetime
 
+TERMINATION_CODES = [
+	"success",
+	"nokey",
+	"noclient"
+]
+
 def send(ip: str, port: int, data: bytes):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((ip, port))
 		s.send(data)
-		recived = "success" if int.from_bytes(s.recv(1), "big") else "failure"
+		termination_code = int.from_bytes(s.recv(1), "big")
+		recived = TERMINATION_CODES[termination_code]
 		s.close()
 		print(recived)
 		fprint(f"{data}\t{recived}")
 	except Exception as e:
+		print("unknown")
 		fprint(e, traceback.format_exc())
 	return
 
