c1db4bc7ffab40ee1ff639391079fab666f41227
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-18 12:50:06 -0400

    convert bytes to int before if

--- send.py
+++ send.py
@@ -7,7 +7,7 @@ def send(ip: str, port: int, data: bytes):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((ip, port))
 		s.send(data)
-		recived = "success" if s.recv(1) else "failure"
+		recived = "success" if int.from_bytes(s.recv(1), "big") else "failure"
 		s.close()
 		print(recived)
 		fprint(f"{data}\t{recived}")
