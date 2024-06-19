b5dbb57755a83cadfaa5208b7fe6fcab274f6b03
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-19 16:58:24 -0400

    Switching to linking-key

--- send.py
+++ send.py
@@ -2,22 +2,33 @@ import socket
 import traceback
 from datetime import datetime
 
-TERMINATION_CODES = [
+RESPONSE_CODES = [
 	"success",
 	"nokey",
-	"noclient"
+	"noclient",
+	"expired"
 ]
 
-def send(ip: str, port: int, data: bytes):
+def send(ip: str, port: int, data: str, activity: str):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((ip, port))
-		s.send(data)
-		termination_code = int.from_bytes(s.recv(1), "big")
-		recived = TERMINATION_CODES[termination_code]
+		if activity == "login":
+			data = bytes(data, 'utf-8')
+			s.send(b'\x00' + data)
+			key = s.recv(18)
+			response_code = int.from_bytes(s.recv(1), "big")
+			print(key)
+		elif activity == "send":
+			data = int(data[:3]).to_bytes(1,"little") + bytes(data[3:], 'utf-8')
+			s.send(b'\x01' + data)
+			response_code = int.from_bytes(s.recv(1), "big")
+		else:
+			raise Exception(f"Bad activity: {activity}")
+		recieved = RESPONSE_CODES[response_code]
 		s.close()
-		print(recived)
-		fprint(f"{data}\t{recived}")
+		print(recieved)
+		fprint(f"{activity}\t{data}\t{recieved}")
 	except Exception as e:
 		print("unknown")
 		fprint(e, traceback.format_exc())
